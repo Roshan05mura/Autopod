e442e992363159ad281953036bc2df7e98339816
Roshan05mura <75969846+Roshan05mura@users.noreply.github.com>
2021-08-25 17:47:04 +0530

    Create changeTest.sh

---
+++ changeTest.sh
@@ -0,0 +1,9 @@
+sed -i 's/dm.rapidgrant.net/ab.rapidgrant.net/g' test6.yml
+sed -i 's/dm-k8s/ab-k8s/gI' test6.yml
+sed -i 's/dm-app/ab-app/gI' test6.yml
+sed -i 's/dm-app-service/ab-app-service/gI' test6.yml
+sed -i 's/dm-k8s-stateful/ab-k8s-stateful/gI' test6.yml
+sed -i 's/dm-app-secrets/ab-app-secrets/gI' test6.yml
+sed -i 's/msql/new-text/gI' test6.yml
+
+
