cf0012b51c84c1dc7e7124257a7474ac655883e1
Roshan05mura <75969846+Roshan05mura@users.noreply.github.com>
2021-08-25 19:52:32 +0530

    Update changeTest.sh

--- changeTest.sh
+++ changeTest.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sed -i "s/dm.rapidgrant.net/ab.rapidgrant.net/g" test6.yml > test${1}.yml
-sed -i "s/dm-k8s/ab-k8s/gI" test6.yml > test${1}.yml
-sed -i "s/dm-app/ab-app/gI" test6.yml > test${1}.yml
-sed -i "s/dm-app-service/ab-app-service/gI" test6.yml > test${1}.yml
-sed -i "s/dm-k8s-stateful/ab-k8s-stateful/gI" test6.yml > test${1}.yml
-sed -i "s/dm-app-secrets/ab-app-secrets/gI" test6.yml > test${1}.yml
-sed -i "s/msql/AWSRDS4/gI" test6.yml > test${1}.yml
+cat test6.yml > test7.yml
+sed -i "s/dm.rapidgrant.net/ab.rapidgrant.net/g" test7.yml
+sed -i "s/dm-k8s/ab-k8s/gI" test7.yml
+sed -i "s/dm-app/ab-app/gI" test7.yml
+sed -i "s/dm-app-service/ab-app-service/gI" test7.yml
+sed -i "s/dm-k8s-stateful/ab-k8s-stateful/gI" test7.yml
+sed -i "s/dm-app-secrets/ab-app-secrets/gI" test7.yml
+sed -i "s/msql/AWSRDS4/gI" test7.yml
 
 
