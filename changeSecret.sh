dbcbc4831e68c29fbe81d23c30b20a5fc9571e49
Roshan05mura <75969846+Roshan05mura@users.noreply.github.com>
2021-08-25 17:58:21 +0530

    Update changeSecret.sh

--- changeSecret.sh
+++ changeSecret.sh
@@ -1,2 +1,2 @@
-sed -i 's/dm-app-secrets/ab-app-secrets/g' test6.yml
-sed -i 's/msql/AWSRDS4/gI' test6.yml
+sed -i 's/dm-app-secrets/ab-app-secrets/g' secret6.yml
+sed -i 's/msql/AWSRDS4/gI' secret6.yml
