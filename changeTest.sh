efb2fbc787871d8f4c68a078d1c7ea329c59646d
Roshan05mura <75969846+Roshan05mura@users.noreply.github.com>
2021-08-25 18:29:38 +0530

    Update changeTest.sh

--- changeTest.sh
+++ changeTest.sh
@@ -1,11 +1,11 @@
 val=7
 
-sed -i 's/dm.rapidgrant.net/ab.rapidgrant.net/g' test6.yml > test${val}.yml
-sed -i 's/dm-k8s/ab-k8s/gI' test6.yml > test${val}.yml
-sed -i 's/dm-app/ab-app/gI' test6.yml > test${val}.yml
-sed -i 's/dm-app-service/ab-app-service/gI' test6.yml > test${val}.yml
-sed -i 's/dm-k8s-stateful/ab-k8s-stateful/gI' test6.yml > test${val}.yml
-sed -i 's/dm-app-secrets/ab-app-secrets/gI' test6.yml > test${val}.yml
-sed -i 's/msql/AWSRDS4/gI' test6.yml > test${val}.yml
+sed -i "s/dm.rapidgrant.net/ab.rapidgrant.net/g" test6.yml > test${val}.yml
+sed -i "s/dm-k8s/ab-k8s/gI" test6.yml > test${val}.yml
+sed -i "s/dm-app/ab-app/gI" test6.yml > test${val}.yml
+sed -i "s/dm-app-service/ab-app-service/gI" test6.yml > test${val}.yml
+sed -i "s/dm-k8s-stateful/ab-k8s-stateful/gI" test6.yml > test${val}.yml
+sed -i "s/dm-app-secrets/ab-app-secrets/gI" test6.yml > test${val}.yml
+sed -i "s/msql/AWSRDS4/gI" test6.yml > test${val}.yml
 
 
