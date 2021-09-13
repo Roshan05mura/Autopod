dc22c57d65250f79f01dd50cd5fe3a4bebac2805
Roshan05mura <75969846+Roshan05mura@users.noreply.github.com>
2021-09-13 15:59:07 +0530

    Update changeTest.sh

--- changeTest.sh
+++ changeTest.sh
@@ -4,7 +4,7 @@ sed -i "s/dm.rapidgrant.net/ab.rapidgrant.net/g" test7.yml
 sed -i "s/dm-k8s/ab-k8s/gI" test7.yml
 sed -i "s/dm-app/ab-app/gI" test7.yml
 sed -i "s/dm-data/ab-data/gI" test7.yml
-sed -i "s/dm-endpoint/ab-endpoint/gI" test7.yml
+sed -i "s/db-endpoint/ab-endpoint/gI" test7.yml
 sed -i "s/dm-app-service/ab-app-service/gI" test7.yml
 sed -i "s/dm-k8s-stateful/ab-k8s-stateful/gI" test7.yml
 sed -i "s/dm-app-secrets/ab-app-secrets/gI" test7.yml
