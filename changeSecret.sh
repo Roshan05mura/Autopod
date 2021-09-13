80d39cc5a92851304c57a035b87d1ff0c0860ff9
Roshan05mura <75969846+Roshan05mura@users.noreply.github.com>
2021-09-13 13:43:17 +0530

    Update changeSecret.sh

--- changeSecret.sh
+++ changeSecret.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat secret6.yml > secret7.yml
+cat secret8.yml > secret7.yml
 sed -i "s/dm-app-secrets/ab-app-secrets/g" secret7.yml
-sed -i "s/msql/sys/gI" secret7.yml
+sed -i "s/AWSRDS4/AWSRDS5/gI" secret7.yml
